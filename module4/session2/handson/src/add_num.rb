4a17390d76c37a668f69757940db2f7da059d9ce
jordankusuma <c14180102@john.petra.ac.id>
2021-07-29 01:33:17 +0900

    add new spec and return [4, 0] when input is [3, 9]

--- module4/session2/handson/src/add_num.rb
+++ module4/session2/handson/src/add_num.rb
@@ -5,10 +5,10 @@ class AddNum
         else
             last_index = input.length() - 1
             input.reverse_each do |num|
-                if (more_than_one_digit?(num) && last_index == 0)
+                if (more_than_one_digit?(num) && last_index > 0)
                     input[last_index] = 0
-                    input.unshift(1)
-                else (!more_than_one_digit?(num))
+                    last_index = last_index - 1
+                else
                     input[last_index] = num + 1
                     break
                 end
